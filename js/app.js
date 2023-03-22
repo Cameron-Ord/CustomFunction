64c56e724733ecc7eedbb0a7ec76df43dd35d75c
cameron <hammerbro_97@hotmail.com>
2023-03-21 19:23:43 -0600

    added more functions

--- js/app.js
+++ js/app.js
@@ -4,3 +4,22 @@ function generic_greeting(){
     console.log(`hello!`);
 
 }
+
+generic_greeting();
+
+function specific_greeting(){
+
+    let user = `cameron`;
+
+    if(user == `cameron`){
+
+        console.log(`specific greeting`)
+    }
+
+
+
+}
+
+specific_greeting();
+
+
