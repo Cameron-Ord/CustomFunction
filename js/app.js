7a203f889f45c8f4b309082254bc28c3f5777eb5
cameron <hammerbro_97@hotmail.com>
2023-03-21 19:56:01 -0600

    function array

--- js/app.js
+++ js/app.js
@@ -20,3 +20,20 @@ specific_greeting(`alex`);
 specific_greeting(`bruce`);
 specific_greeting(`bowser`);
 specific_greeting(`ronald`);
+
+
+function filter_winning_users(user_array) { 
+
+
+    for(let counter = 0; counter < user_array.length; counter++){
+
+        if(user_array[counter].includes(`winner`)){
+            return user_array[counter];
+        }
+    }
+
+}
+
+let first_winner = filter_winning_users([`loser`, `almost`, `best_winner_user`]);
+
+
